d95654a8c911589b11990dab67adea98096d6808
Purshottam Paitdar <pankajpatidar9521@gmail.com>
2023-12-18 20:16:39 +0530

    commit done

--- src/main/java/com/example/Socialplatform/repository/PostRepository.java
+++ src/main/java/com/example/Socialplatform/repository/PostRepository.java
@@ -1,11 +1,8 @@
 package com.example.Socialplatform.repository;
 
 import com.example.Socialplatform.model.Post;
-import com.example.Socialplatform.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
 public interface PostRepository extends JpaRepository<Post,Long> {
-    List<Post> findByUserOrderByCreatedDataDesc(User user);
+
 }
